309f7bbd09a4abdd969cefeab040e91bf8147997
akobir9990 <akobir.qoshshayev@gmail.com>
2023-07-24 15:45:00 +0500

    some changes

--- js/main.js
+++ js/main.js
@@ -32,3 +32,17 @@ $(".owl-carousel").owlCarousel({
   margin: 10,
   nav: true,
 });
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
